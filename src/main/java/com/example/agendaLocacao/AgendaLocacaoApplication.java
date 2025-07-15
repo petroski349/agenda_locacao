d3b0f9ca39a684fba900f79922687cbb9ce73214
Diogo Petroski <diogopetroski@gmail.com>
2025-07-14 23:06:15 -0300

    criação do projeto

---
+++ src/main/java/com/example/agendaLocacao/AgendaLocacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendaLocacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaLocacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaLocacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agendaLocacao/AgendaLocacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendaLocacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaLocacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
